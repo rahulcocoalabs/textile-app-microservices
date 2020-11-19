7126b5350e290f12e4318ae4f04f04fa0780a772
rakeshcocoalabs <rakesh@cocoalabs.in>
2020-11-19 15:01:41 +0530

    f

--- app/controllers/review.controller.js
+++ app/controllers/review.controller.js
@@ -45,7 +45,7 @@ var otpConfig = paramsConfig.otp;
                 tSModifiedAt: null
             });
 
-            return res.send(review);
+            return res.send(user);
             var save_review = await review.save();
             return res.send({
                 success: 1,
